604cbd4f30a6363c5e95842a888e5c48b7ccfb0c
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-04-26 12:20:15 -0300

    ✨ adds cors

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,16 @@
 import "dotenv/config";
 import express, { json } from "express";
 import { distributeValue } from "./services.mjs";
+import cors from "cors";
 const app = express();
 const port = process.env.PORT;
 
 app.use(json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 
 app.get("/", (_, res) => {
   res.json("Hello world");
