ee0d1293d5ae75032beb79ad0a2fe87da4279804
Pedro Santos <pedrosantosdevelop@gmail.com>
2022-05-06 10:12:07 -0300

    🔨 creates deploy script

--- scripts.sh
+++ scripts.sh
@@ -1,9 +1,11 @@
-if [ ! -z "$1" ] && [ ! -z "$2" ] && [ "$1" = "upgrade" ]; then
+if [ ! -z "$1" ] && [ ! -z "$2" ] && [ "$1" = "upgrade-nocodb-deploy" ]; then
   heroku container:login
   docker pull nocodb/nocodb:latest
   docker tag nocodb/nocodb:latest registry.heroku.com/"$2"/web
   docker push registry.heroku.com/"$2"/web
   heroku container:release -a "$2" web
+elif [ ! -z "$1" ] && [ "$1" = "deploy-integration" ]; then
+  git push heroku main
 else
   echo "Missing/invalid parameter to execute scripts.sh"
 fi
